077964bec13f641a9c89e809ef905c14792f32e7
yangdawei <yangdawei.hit@gmail.com>
2020-09-12 12:37:22 +0800

    course-02

--- README.md
+++ README.md
@@ -15,7 +15,19 @@
 
 ### 第2次
 
-- 时间：2020年9月10日
+- 时间：2020年9月12日9:00-11:00
+- 地点: B7216
 - 内容
+  - Python安装
+  - VS Code及Python插件安装
+  - Python-100天项目Day01-15: 02
+- 作业
+  - [Python-100天项目](https://github.com/jackfrued/Python-100-Days)Day01-15: 03 - 06
+  - [Python教程700集](https://www.bilibili.com/video/BV1QE411x76A?p=1): P1 - P40
+
+
+### 第3次
+
+- 待定内容
   - [pip.ini](C:\Users\ThinkPad\pip)
   - [site-packages](C:\Python38\Lib\site-packages)
--- e1.py
+++ e1.py
@@ -1,1 +1,59 @@
-print("hello Python!")
+# a = 321
+# b = 12
+# print(a + b)    # 333
+# print(a - b)    # 309
+# print(a * b)    # 3852
+# print(a / b)    # 26.75
+
+# True
+# False
+
+# # sequence
+# list
+# tuple
+# dict
+# set
+
+# numpy
+# scipy
+# matploblib
+# pandas
+# a = 100
+# b = 12.345
+# c = 1 + 5j
+# d = 'hello, world'
+# e = True
+# b = str(a)
+# print(type(str(a)))    # <class 'int'>
+# print(type(b))    # <class 'float'>
+# print(type(c))    # <class 'complex'>
+# print(type(d))    # <class 'str'>
+# print(type(int(d)))    # <class 'str'>
+# print(type(e))    o# <class 'bool'>
+"""
+a = float(input('a = '))
+b = float(input('b = '))
+print('%d + %d = %d' % (a, b, a + b))
+print('{2:>5.3f} = {1:>8.3f}+ {0:>8.3f}'.format(a, b, a + b))
+print('%d - %d = %d' % (a, b, a - b))
+print('%d * %d = %d' % (a, b, a * b))
+print('%d / %d = %f' % (a, b, a / b))
+print('%d // %d = %d' % (a, b, a // b))
+print('%d %% %d = %d' % (a, b, a % b))
+print('%d ** %d = %d' % (a, b, a ** b))
+"""
+
+# a = 10
+# b = 3
+# a += b        # 相当于：a = a + b
+# a *= a + 2    # 相当于：a = a * (a + 2)
+# print(a)      # 算一下这里会输出什么
+"""
+将华氏温度转换为摄氏温度
+
+Version: 0.1
+Author: 骆昊
+"""
+f = float(input('请输入华氏温度: '))
+c = (f - 32) / 1.8
+print('%.1f华氏度 = %.1f摄氏度' % (f, c))
